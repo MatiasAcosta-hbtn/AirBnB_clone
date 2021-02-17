d64259f518654cfde7921463da6e8ffbfeadc0bf
Matias Acosta <2277@holbertonschool.com>
2021-02-17 00:06:07 -0300

    Initial commit with tasks 0-12

---
+++ tests/test_file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Tests File"""
+import unittest
+from models.engine.file_storage import FileStorage
+import pep8
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test of Amenity_class"""
