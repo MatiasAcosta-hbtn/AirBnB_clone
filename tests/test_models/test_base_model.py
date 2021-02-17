b0d0b951d12c3cbd5e7fb54af27ea61931a7e9ec
martinmsaavedra <martin@bildung.uy>
2021-02-17 17:05:21 +0000

    Fix tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from uuid import uuid4
 import pep8
 
-
+'''
 class TestBaseModel(unittest.TestCase):
     """Test of Base Model class"""
 
@@ -43,7 +43,7 @@ class TestBaseModel(unittest.TestCase):
     def test_id(self):
         """test if uuid is a string"""
         new = BaseModel()
-        self.assertEqual(type(new.id), str)
+        self.assertIsInstance(new.id, str)
 
     def test_created_at(self):
         """test created_at format"""
@@ -54,6 +54,49 @@ class TestBaseModel(unittest.TestCase):
         """test updated_at format"""
         new = BaseModel()
         self.assertIsInstance(new.updated_at, datetime)
+'''
+"""Unittest for base model module."""
 
+
+class TestBaseModel(unittest.TestCase):
+    """ test the base model class"""
+
+    def test_base_model_id_format(self):
+        """test if UUID is a string"""
+        id_nbr = BaseModel()
+        self.assertIsInstance(id_nbr.id, str)
+
+    def test_base_model_created_at_format(self):
+        """test if created_at is datetime format"""
+        date = BaseModel()
+        self.assertIsInstance(date.created_at, datetime)
+
+    def test_base_model_updated_at_format(self):
+        """test if date and time updated are in datetime format"""
+        date = BaseModel()
+        self.assertIsInstance(date.updated_at, datetime)
+
+    def test_init(self):
+        """Test instantiation"""
+        instance = BaseModel()
+        self.assertIs(type(instance), BaseModel)
+        instance.name = "accomodation"
+        attributes_types = {"id": str, "created_at": datetime,
+                            "updated_at": datetime, "name": str}
+        self.assertEqual(instance.name, "accomodation")
+        self.assertIs(type(attributes_types), dict)
+
+    def test_str(self):
+        """test if the str method has the correct output"""
+        instance = BaseModel()
+        string = "[BaseModel] ({}) {}".format(instance.id, instance.__dict__)
+        self.assertEqual(string, str(instance))
+
+    def test_updated_at_after_save(self):
+        """test if updated_at has the current datetime after save"""
+        date = BaseModel()
+        date_before_save = date.updated_at
+        date.save()
+        self.assertTrue(date.updated_at > date_before_save)
 if __name__ == "__main__":
     unittest.main()
